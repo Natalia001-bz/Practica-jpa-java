5ca75c31a3f0b55d35f0f243b56ed10417e5bb41
Natalia001-bz <mesa.cc1257@gmail.com>
2025-02-18 10:39:58 -0500

    implementación de métodos

--- src/main/java/com/JpaClase/Jpa/service/IPersonaService.java
+++ src/main/java/com/JpaClase/Jpa/service/IPersonaService.java
@@ -1,4 +1,17 @@
 package com.JpaClase.Jpa.service;
 
+import com.JpaClase.Jpa.model.Persona;
+import java.util.List;
+
 public interface IPersonaService {
+
+    public List<Persona> getPersonas();
+
+    public void savePersona (Persona persona);
+
+    public void deletePersona(Long id);
+
+    public  Persona findPersona(Long id);
+
+    public void  editarPersona(Long id, Persona  personaActualizada);
 }
--- src/main/java/com/JpaClase/Jpa/service/PersonaService.java
+++ src/main/java/com/JpaClase/Jpa/service/PersonaService.java
@@ -1,7 +1,50 @@
 package com.JpaClase.Jpa.service;
 
+import com.JpaClase.Jpa.model.Persona;
+import com.JpaClase.Jpa.repository.IPersonaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-@Service
-public class PersonaService {
 
+import java.util.List;
+
+@Service
+public class PersonaService implements IPersonaService {
+@Autowired
+private IPersonaRepository personaRepository;
+
+    @Override
+    public List<Persona> getPersonas() {
+        List<Persona> listaPersonas = personaRepository.findAll();
+        return listaPersonas;
+    }
+
+    @Override
+    public void savePersona(Persona perso) {
+personaRepository.save(perso);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+personaRepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+Persona perso = personaRepository.findById(id).orElse(null);
+        return null;
+    }
+
+    @Override
+    public void editarPersona(Long id, Persona personaActualizada) {
+        Persona personaExistente = personaRepository.findById(id).orElse(null);
+
+        if(personaExistente != null){
+            personaExistente.setNombre(personaActualizada.getNombre());
+            personaExistente.setApellido(personaActualizada.getApellido());
+            personaExistente.setEdad(personaActualizada.getEdad());
+            personaRepository.save(personaExistente);
+        }else{
+            System.out.println("No se encontró persona con el id: " +  id);
+        }
+    }
 }
